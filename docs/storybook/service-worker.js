b02f5287ca3bb2c24b5dc3938c6c72746e8519d2
m-cabello <m-cabello@users.noreply.github.com>
2021-05-30 20:40:11 +0000

    Deploying to main from @ ucsb-cs156-s21/proj-ucsb-cs-las@91629d66c528c7781d861d26f284af3ec573f996 🚀

--- docs/storybook/service-worker.js
+++ docs/storybook/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.4bd3faccc2fca35dbb11f654275a05dd.js"
+  "precache-manifest.4af1e1317d218d10f76395f090c5a340.js"
 );
 
 self.addEventListener('message', (event) => {
