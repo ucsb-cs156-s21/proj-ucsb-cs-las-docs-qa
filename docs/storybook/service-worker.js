025dbdd38b63e7c597ea91404b7ddc0b37650e54
pconrad <pconrad@users.noreply.github.com>
2021-05-30 17:14:05 +0000

    Deploying to main from @ ucsb-cs156-s21/proj-ucsb-cs-las@d1a829db082f9c6d120b1b61a852a3bc0195fda9 🚀

--- docs/storybook/service-worker.js
+++ docs/storybook/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.782aa71eee4845deffc4604207711c3e.js"
+  "precache-manifest.b0c676adf06ad645111cfb0e645806b1.js"
 );
 
 self.addEventListener('message', (event) => {
