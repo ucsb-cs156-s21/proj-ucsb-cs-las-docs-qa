721c3953ff36b2621559c3db371b7ce1e3c52639
Zwang1680 <Zwang1680@users.noreply.github.com>
2021-06-03 00:40:18 +0000

    Deploying to main from @ ucsb-cs156-s21/proj-ucsb-cs-las@bec32329bc75cb9fd98dd7082f3e184d4220a5ee 🚀

--- docs/storybook/service-worker.js
+++ docs/storybook/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.e472553cbc13df3f8575daeff47aac9c.js"
+  "precache-manifest.b848e51703725333920ff69a5f3111f9.js"
 );
 
 self.addEventListener('message', (event) => {
